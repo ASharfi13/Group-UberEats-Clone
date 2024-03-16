9d5c603f73457566a631722a6e6801826ffe9b26
Khobe Arzadon <khobearzadon@yahoo.com>
2024-03-15 22:34:57 -0700

    added some seed_data

--- aa19-python-group-project/app/seeds/seed_data.py
+++ aa19-python-group-project/app/seeds/seed_data.py
@@ -1,14 +1,105 @@
 users= [
     {
-        "username": "blah",
-        "email": "blah",
-        "password": "blah",
+        "name": "John White",
+        "email": "jwhite@gmail.com",
+        "password": "jwpassword12",
     },
     {
-        "username": "blah",
-        "email": "blah",
-        "password": "blah",
+        "name": "Jane Wallen",
+        "email": "wallenj@yahoo.com",
+        "password": "foodlover243",
+    },
+    {
+        "name": "Mike Wazowski",
+        "email": "googlybear@hotmail.com",
+        "password": "scarymonster564",
+    },
+    {
+        "name": "James Sullivan",
+        "email": "boomonster@gmail.com",
+        "password": "purpledotsonme32",
+    },
+    {
+        "name": "Donald Duck",
+        "email": "quackquack@yahoo.com",
+        "password": "ilovedaisy45",
+    },
+    {
+        "name": "Daisy Duck",
+        "email": "ballerina@hotmail.com",
+        "password": "daisyismyname278",
+    },
+    {
+        "name": "Peter Pan",
+        "email": "neverland@gmail.com",
+        "password": "captainhookiscrazy13",
+    },
+    {
+        "name": "Bud Robinson",
+        "email": "inventions@yahoo.com",
+        "password": "tothefuture732",
+    },
+    {
+        "name": "Toby Turtle",
+        "email": "journeysawait@hotmail.com",
+        "password": "slowwinstherace27",
+    },
+    {
+        "name": "Jack Sparrow",
+        "email": "richesgalore@gmail.com",
+        "password": "iamapirate68",
+    },
+    {
+        "name": "Buzz Lightyear",
+        "email": "toinfinity@yahoo.com",
+        "password": "beyondspace694",
+    },
+    {
+        "name": "Luke Skywalker",
+        "email": "theforce@hotmail.com",
+        "password": "r2d23cpo",
+    },
+    {
+        "name": "Han Solo",
+        "email": "memyselfandi@gmail.com",
+        "password": "iloveleia678",
+    },
+    {
+        "name": "Mace Windu",
+        "email": "purplesaber@yahoo.com",
+        "password": "republic532",
+    },
+    {
+        "name": "Ahsoka Tano",
+        "email": "thebestpadawan@hotmail.com",
+        "password": "goodoverevil832",
+    },
+    {
+        "name": "Doc Hudson",
+        "email": "oldcar@gmail.com",
+        "password": "iloveracing947",
+    },
+    {
+        "name": "Carl Fredricksen",
+        "email": "oldman@yahoo.com",
+        "password": "traveltheworld453",
+    },
+    {
+        "name": "Charles Muntz",
+        "email": "travelerofeverywhere@hotmail.com",
+        "password": "paradisefalls238",
+    },
+    {
+        "name": "Mei Lee",
+        "email": "redpanda@gmail.com",
+        "password": "fourtown4life",
+    },
+    {
+        "name": "Barley Lightfoot",
+        "email": "wizardmaster@yahoo.com",
+        "password": "wheresdad452",
     }
+
 ]
 
 
@@ -26,24 +117,398 @@ restaurants= [
         "type": "Chinese",
         "owner_id": 3,
         "imageUrl": "blah"
+    },
+    {
+        "name": "Burger King",
+        "location": "46 Queen Ave",
+        "type": "American",
+        "owner_id": 4,
+        "imageUrl": "blah"
+    },
+    {
+        "name": "Manila Express",
+        "location": "22 Bataan St",
+        "type": "Filipino",
+        "owner_id": 5,
+        "imageUrl": "blah"
+    },
+    {
+        "name": "Mcdonalds ",
+        "location": "62 Double Dr",
+        "type": "American",
+        "owner_id": 6,
+        "imageUrl": "blah"
+    },
+    {
+        "name": "Kevins Noodle House",
+        "location": "12 Sunset Blvd",
+        "type": "Vietnamese",
+        "owner_id": 7,
+        "imageUrl": "blah"
+    },
+    {
+        "name": "Naan Curry",
+        "location": "89 Fandango Way",
+        "type": "Indian",
+        "owner_id": 8,
+        "imageUrl": "blah"
+    },
+    {
+        "name": "Maggie's Tacos",
+        "location": "346 Cancun Ave",
+        "type": "Mexican",
+        "owner_id": 9,
+        "imageUrl": "blah"
+    },
+    {
+        "name": "White Elephant",
+        "location": "67 Lady St",
+        "type": "Thai",
+        "owner_id": 10,
+        "imageUrl": "blah"
+    },
+    {
+        "name": "Pocha House",
+        "location": "71 Main Blvd",
+        "type": "Korean",
+        "owner_id": 1,
+        "imageUrl": "blah"
     }
 ]
 
 menu_items=[
     {
-        "name": "blah",
-        "type": "blah",
-        "price": number,
-        "restaurant_id": number,
+        "name": "Large Pepperoni Pizza",
+        "type": "Pizza",
+        "price": 20.99,
+        "restaurant_id": 1,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Orange Chicken",
+        "type": "Chicken",
+        "price": 8.99,
+        "restaurant_id": 2,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Cheeseburger",
+        "type": "Burger",
+        "price": 5.99,
+        "restaurant_id": 3,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Sinigang",
+        "type": "Soup",
+        "price": 9.99,
+        "restaurant_id": 4,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Hamburger",
+        "type": "Burger",
+        "price": 4.99,
+        "restaurant_id": 5,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Beef and Noodles Pho",
+        "type": "Soup",
+        "price": 8.99,
+        "restaurant_id": 6,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Chicken Curry",
+        "type": "Soup",
+        "price": 7.99,
+        "restaurant_id": 7,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Carne Asada Taco",
+        "type": "Taco",
+        "price": 2.99,
+        "restaurant_id": 8,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Pad Thai Noodle",
+        "type": "Noodle",
+        "price": 10.99,
+        "restaurant_id": 9,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Beef Bulgogi",
+        "type": "Beef",
+        "price": 9.99,
+        "restaurant_id": 10,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Medium Cheese Pizza",
+        "type": "Pizza",
+        "price": 12.99,
+        "restaurant_id": 1,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Pork Fried Rice",
+        "type": "Rice",
+        "price": 6.99,
+        "restaurant_id": 2,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Chicken Nuggets",
+        "type": "Chicken",
+        "price": 3.99,
+        "restaurant_id": 3,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Adobo",
+        "type": "Soup",
+        "price": 8.99,
+        "restaurant_id": 4,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Breakfast Burrito",
+        "type": "Burrito",
+        "price": 2.99,
+        "restaurant_id": 5,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Pork Chop Over Rice",
+        "type": "Pork",
+        "price": 9.99,
+        "restaurant_id": 6,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Butter Chicken",
+        "type": "Chicken",
+        "price": 10.99,
+        "restaurant_id": 7,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Al Pastor Burrito",
+        "type": "Burrito",
+        "price": 12.99,
+        "restaurant_id": 8,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Crab Fried Rice",
+        "type": "Rice",
+        "price": 13.99,
+        "restaurant_id": 9,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Pork Belly",
+        "type": "Pork",
+        "price": 7.99,
+        "restaurant_id": 10,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Buffalo Wings",
+        "type": "Chicken",
+        "price": 10.99,
+        "restaurant_id": 1,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Honey Walnut Shrimp",
+        "type": "Shrimp",
+        "price": 5.99,
+        "restaurant_id": 2,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Chicken Sandwich",
+        "type": "Chicken",
+        "price": 6.99,
+        "restaurant_id": 3,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Pancit",
+        "type": "Noodle",
+        "price": 7.99,
+        "restaurant_id": 4,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Hash Browns",
+        "type": "Potato",
+        "price": 4.99,
+        "restaurant_id": 5,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Chicken Noodle Pho",
+        "type": "Soup",
+        "price": 7.99,
+        "restaurant_id": 6,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Chana Masala",
+        "type": "Soup",
+        "price": 8.99,
+        "restaurant_id": 7,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Birria Taco",
+        "type": "Taco",
+        "price": 6.99,
+        "restaurant_id": 8,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Yellow Curry",
+        "type": "Soup",
+        "price": 11.99,
+        "restaurant_id": 9,
+        "imageUrl": "blah",
+    },
+    {
+        "name": "Kimchi Fried Rice",
+        "type": "Rice",
+        "price": 11.99,
+        "restaurant_id": 10,
         "imageUrl": "blah",
     }
 ]
 
 reviews=[
     {
-        "stars": number,
-        "description": "long review goes here",
-        "restaurant_id": number,
+        "stars": 4,
+        "description": "The food was great and we received excellent service here.",
+        "restaurant_id": 3,
+        "user_id": 1
+    },
+    {
+        "stars": 3,
+        "description": "The food was not what I was expecting. I think I liked it.",
+        "restaurant_id": 4,
+        "user_id": 2
+    },
+    {
+        "stars": 2,
+        "description": "Not a big fan of this place, but it did fill me up.",
+        "restaurant_id": 5,
+        "user_id": 3
+    },
+    {
+        "stars": 5,
+        "description": "The noodles were amazing. I would order from here again.",
+        "restaurant_id": 6,
+        "user_id": 4
+    },
+    {
+        "stars": 4,
+        "description": "The food was great and I loved the spices in the food.",
+        "restaurant_id": 7,
+        "user_id": 5
+    },
+    {
+        "stars": 1,
+        "description": "Tacos are not my thing. I do not know why I ordered from here.",
+        "restaurant_id": 8,
+        "user_id": 6
+    },
+    {
+        "stars": 5,
+        "description": "I always order from here and they do not disappoint.",
+        "restaurant_id": 9,
+        "user_id": 7
+    },
+    {
+        "stars": 3,
+        "description": "My first time trying this place out and it was good.",
+        "restaurant_id": 10,
+        "user_id": 8
+    },
+    {
+        "stars": 1,
+        "description": "There was way too much cheese on my pizza.",
+        "restaurant_id": 1,
+        "user_id": 9
+    },
+    {
+        "stars": 4,
+        "description": "This is my go to spot every single time.",
+        "restaurant_id": 2,
+        "user_id": 10
+    },
+    {
+        "stars": 2,
+        "description": "They did not give me that many fries and I ordered a lot.",
+        "restaurant_id": 3,
+        "user_id": 11
+    },
+    {
+        "stars": 5,
+        "description": "I love their food and I am so happy when I eat here.",
+        "restaurant_id": 4,
+        "user_id": 12
+    },
+    {
+        "stars": 2,
+        "description": "This food sucks. I am never eating here again.",
+        "restaurant_id": 5,
+        "user_id": 13
+    },
+    {
+        "stars": 5,
+        "description": "Their food makes me feel nice and toasty. It is amazing.",
+        "restaurant_id": 6,
+        "user_id": 14
+    },
+    {
+        "stars": 3,
+        "description": "Little too spicy for me, but it was good.",
+        "restaurant_id": 7,
+        "user_id": 15
+    },
+    {
+        "stars": 5,
+        "description": "The tacos were so good. I have never had that many tacos.",
+        "restaurant_id": 8,
+        "user_id": 16
+    },
+    {
+        "stars": 5,
+        "description": "The food was the best and we received good service here.",
+        "restaurant_id": 9,
+        "user_id": 17
+    },
+    {
+        "stars": 3,
+        "description": "New Place and it was okay.",
+        "restaurant_id": 10,
+        "user_id": 18
+    },
+    {
+        "stars": 5,
+        "description": "I love the pizza here. It is great",
+        "restaurant_id": 1,
+        "user_id": 19
+    },
+    {
+        "stars": 2,
+        "description": "This food is okay. It could have been better",
+        "restaurant_id": 2,
+        "user_id": 20
     }
 ]
 
