4d2884de7972852b608710663aae57287fa60922
Khobe Arzadon <khobearzadon@yahoo.com>
2024-03-16 16:59:17 -0700

    added menu_item routes

--- aa19-python-group-project/app/api/menu_item_routes.py
+++ aa19-python-group-project/app/api/menu_item_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, Flask, request
 # from app.posts import users, posts
 from app.models import db, MenuItem
 import json
@@ -16,9 +16,35 @@ def getAllDetails():
 # create a menu item
 #/restaurants/<int:restaurantId>/menu-items
 # this might have to go IN THE RESTAURANTS ROUTES
+@menu_item_routes.route("/restaurant/<int:id>/menu-items", methods=["POST"])
+def createMenuItem(id):
+    data = request.json
+    newItem = MenuItem(**data, restaurant_id=id)
+    db.session.add(newItem)
+    db.session.commit()
+    return json.dumps(newItem.toDict())
+
 
 # edit a menu item
 # /<int:itemId>
+@menu_item_routes.route("/<int:id>", methods=["PUT"])
+def updateMenuItem(id):
+    item = MenuItem.query.get(id)
+    newData = request.json
+    for keys in newData:
+        setattr(item, keys, newData.get(keys))
+    db.session.commit()
+    return json.dumps(item.toDict())
+
+
 
 # delete a menu item
 # /<int:itemId>
+@app.route("/<int:id>", methods=["DELETE"])
+def deleteMenuItem(id):
+    item = MenuItem.query.get(id)
+    db.session.delete(item)
+    db.session.commit()
+    return json.dumps({
+        "id": id
+    })
