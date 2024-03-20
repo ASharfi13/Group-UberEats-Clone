69d94dd7a6a74a322c05feab0bc053adb4df7809
Khobe Arzadon <khobearzadon@yahoo.com>
2024-03-19 22:41:44 -0700

    added

--- react-vite/src/components/Restaurant/SingleRestaurant.jsx
+++ react-vite/src/components/Restaurant/SingleRestaurant.jsx
@@ -106,7 +106,6 @@ function SingleRestaurant() {
                         restaurantId={restaurantId}
                       />
                     </>
-                    )}
                   </div>
                 </div>
               ))}
