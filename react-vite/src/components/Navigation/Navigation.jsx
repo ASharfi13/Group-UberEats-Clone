601615879f52e3cda478ca74817e4a2a5570f512
Khobe Arzadon <khobearzadon@yahoo.com>
2024-03-21 07:59:34 -0700

    added

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -4,8 +4,13 @@ import CartButton from "./CartButton";
 import "./Navigation.css";
 
 function Navigation() {
+  const handleFeature = (e) => {
+    console.log("feature coming soon", e);
+    alert("Feature Coming Soon");
+  };
+
   return (
-    <ul>
+    <ul className="nav-bar">
       <div className="left-side-nav-bar">
         <NavLink to="/">
           <img
@@ -14,9 +19,23 @@ function Navigation() {
             alt="logi"
           />
         </NavLink>
+        <button className="delivery-pickup">
+          <button className="button-delivery" onClick={handleFeature}>
+            Delivery
+          </button>
+          <button className="button-pickup" onClick={handleFeature}>
+            Pickup
+          </button>
+        </button>
       </div>
 
       <div className="right-side-nav-bar">
+        <input
+          className="search-bar "
+          type="text"
+          name="search-bar"
+          placeholder="Search for a restaurant"
+        />
         <ProfileButton />
         <CartButton />
       </div>
