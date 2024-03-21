31e693065fe7572875c5cd7cd4e8cbb92f0f9aa0
Khobe Arzadon <khobearzadon@yahoo.com>
2024-03-21 16:17:36 -0700

    added more styling

--- react-vite/src/components/MenuItems/CreateMenuItemForm.jsx
+++ react-vite/src/components/MenuItems/CreateMenuItemForm.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { writeMenuItem, getMenuItemTypes } from "../../redux/menuItemReducer";
 import { useNavigate, useParams } from "react-router-dom";
+import "./UpdateMenuItemForm.css";
 
 export default function MenuItemForm() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((state) => state.session.user);
   const menuItems = useSelector((state) => state.menuItemState);
-  const { restaurantId } = useParams()
+  const { restaurantId } = useParams();
 
   const [name, setName] = useState("");
   const [type, setType] = useState("");
@@ -16,10 +17,10 @@ export default function MenuItemForm() {
   const [image, setImage] = useState("");
   const [errors, setErrors] = useState({});
 
-  const menuItemTypes = useSelector((state) => state.menuItemState.types)
+  const menuItemTypes = useSelector((state) => state.menuItemState.types);
 
   useEffect(() => {
-    dispatch(getMenuItemTypes())
+    dispatch(getMenuItemTypes());
   }, [dispatch]);
 
   const onSubmit = async (e) => {
@@ -33,61 +34,67 @@ export default function MenuItemForm() {
     };
 
     const newItem = await dispatch(writeMenuItem(restaurantId, payload));
-    if (newItem.errors) setErrors(newItem.errors)
-    else navigate(`/restaurants/${restaurantId}`)
+    if (newItem.errors) setErrors(newItem.errors);
+    else navigate(`/restaurants/${restaurantId}`);
   };
 
   return (
     <>
-    {menuItemTypes &&
-    <div>
-      <form onSubmit={onSubmit}>
-        <h1>Create A New Menu Item</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter the name of the item"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            // required
-          ></input>
-          <p className="errors">{errors.name ? errors.name : null}</p>
-        </div>
+      {menuItemTypes && (
+        <div className="update-menu-page">
+          <form className="update-form" onSubmit={onSubmit}>
+            <h1 className="update-menu-title">Create A New Menu Item</h1>
+            <div>
+              <input
+                type="text"
+                placeholder="Enter the name of the item"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                // required
+              ></input>
+              <p className="errors">{errors.name ? errors.name : null}</p>
+            </div>
 
-        <div>
-          <input
-            type="number"
-            placeholder="Enter the price of the item"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            // required
-          ></input>
-          <p className="errors">{errors.price ? errors.price : null}</p>
+            <div>
+              <input
+                type="number"
+                placeholder="Enter the price of the item"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                // required
+              ></input>
+              <p className="errors">{errors.price ? errors.price : null}</p>
+            </div>
+            <div>
+              <select value={type} onChange={(e) => setType(e.target.value)}>
+                <option value={""} disabled selected>
+                  Select Type
+                </option>
+                {menuItemTypes.map((menuItem, idx) => (
+                  <option key={idx}>{menuItem}</option>
+                ))}
+              </select>
+              <p className="errors">{errors.type ? errors.type : null}</p>
+            </div>
+            <div>
+              <input
+                type="url"
+                placeholder="Enter Image Url"
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              ></input>
+              <p className="errors">
+                {errors.imageUrl ? errors.imageUrl : null}
+              </p>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+          <img
+            src="https://i.postimg.cc/0yLWjssc/menu-logo.avif"
+            alt="menu-logo"
+          />
         </div>
-        <div>
-          <select value={type} onChange={(e) => setType(e.target.value)}>
-            <option value={""} disabled selected>
-              Select Type
-            </option>
-            {menuItemTypes.map((menuItem, idx) => (
-              <option key={idx}>{menuItem}</option>
-            ))}
-          </select>
-          <p className="errors">{errors.type ? errors.type : null}</p>
-        </div>
-        <div>
-          <input
-            type="url"
-            placeholder="Enter Image Url"
-            value={image}
-            onChange={(e) => setImage(e.target.value)}
-          ></input>
-          <p className="errors">{errors.imageUrl ? errors.imageUrl : null}</p>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-    }
+      )}
     </>
   );
 }
--- react-vite/src/components/Restaurant/UpdateRestaurantForm.jsx
+++ react-vite/src/components/Restaurant/UpdateRestaurantForm.jsx
@@ -1,103 +1,128 @@
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useNavigate, useParams } from "react-router-dom"
-import { fetchRestaurant, editRestaurant, getRestaurantTypes } from "../../redux/restaurantReducer"
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  fetchRestaurant,
+  editRestaurant,
+  getRestaurantTypes,
+} from "../../redux/restaurantReducer";
+import "./CreateRestaurantForm/CreateRestaurantForm.css";
 
 function UpdateRestaurant() {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const { restaurantId } = useParams()
-    const restaurant = useSelector((state) => state.restaurantState[restaurantId])
-    const restaurantTypes = useSelector((state) => state.restaurantState.types)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { restaurantId } = useParams();
+  const restaurant = useSelector(
+    (state) => state.restaurantState[restaurantId]
+  );
+  const restaurantTypes = useSelector((state) => state.restaurantState.types);
 
-    const [name, setName] = useState(restaurant?.name)
-    const [location, setLocation] = useState(restaurant?.location)
-    const [type, setType] = useState(restaurant?.type)
-    const [image, setImage] = useState(restaurant?.imageUrl)
-    const [errors, setErrors] = useState({})
+  const [name, setName] = useState(restaurant?.name);
+  const [location, setLocation] = useState(restaurant?.location);
+  const [type, setType] = useState(restaurant?.type);
+  const [image, setImage] = useState(restaurant?.imageUrl);
+  const [errors, setErrors] = useState({});
 
+  useEffect(() => {
+    dispatch(fetchRestaurant(restaurantId)).then(
+      dispatch(getRestaurantTypes())
+    );
+    setName(restaurant?.name);
+    setLocation(restaurant?.location);
+    setType(restaurant?.type);
+    setImage(restaurant?.imageUrl);
+  }, [
+    dispatch,
+    restaurantId,
+    restaurant?.name,
+    restaurant?.location,
+    restaurant?.type,
+    restaurant?.imageUrl,
+  ]);
 
-    useEffect(() => {
-        dispatch(fetchRestaurant(restaurantId)).then(dispatch(getRestaurantTypes()))
-        setName(restaurant?.name)
-        setLocation(restaurant?.location)
-        setType(restaurant?.type)
-        setImage(restaurant?.imageUrl)
-    }, [dispatch, restaurantId, restaurant?.name, restaurant?.location, restaurant?.type, restaurant?.imageUrl])
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-    const onSubmit = async (e) => {
-        e.preventDefault()
+    const payload = {
+      name,
+      location,
+      type,
+      imageUrl: image,
+    };
 
-        const payload = {
-            name,
-            location,
-            type,
-            "imageUrl": image
-        }
+    const response = await dispatch(editRestaurant(payload));
+    if (response.errors) setErrors(response.errors);
+    else navigate(`/restaurants/${response.id}`);
+  };
 
-        const response = await dispatch(editRestaurant(payload));
-        if (response.errors) setErrors(response.errors)
-        else navigate(`/restaurants/${response.id}`)
-    }
+  return (
+    <div>
+      {restaurant && restaurantTypes && (
+        <div className="restaurant-page-create">
+          <form className="restaurant-form" onSubmit={onSubmit}>
+            <h1 className="restaurant-title-form">Update Your Restaurant</h1>
+            <div>
+              <input
+                type="text"
+                placeholder="Enter A Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                // required
+              ></input>
+              <p className="restaurant-errors">
+                {errors.name ? errors.name : null}
+              </p>
+            </div>
 
-    return (
-        <div>
-            {restaurant && restaurantTypes && <div>
-                <form onSubmit={onSubmit}>
-                    <h1>Update Your Restaurant</h1>
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Enter A Name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        // required
-                        >
-                        </input>
-                        <p className="errors">{errors.name ? errors.name : null}</p>
-                    </div>
-
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Enter A Location"
-                            value={location}
-                            onChange={(e) => setLocation(e.target.value)}
-                        // required
-                        >
-                        </input>
-                        <p className="errors">{errors.location ? errors.location : null}</p>
-                    </div>
-                    <div>
-                        <select
-                            value={type}
-                            onChange={(e) => setType(e.target.value)}
-                        >
-                            <option value={""} disabled selected>Select Type</option>
-                            {restaurantTypes.map((restaurant, idx) => (
-                                <option key={idx}>{restaurant}</option>
-                            ))}
-                        </select>
-                        <p className="errors">{errors.type ? errors.type : null}</p>
-
-                    </div>
-                    <div>
-                        <input
-                            type="url"
-                            placeholder="Enter New Image Url"
-                            value={image}
-                            onChange={(e) => setImage(e.target.value)}
-                        >
-                        </input>
-                        <p className="errors">{errors.imageUrl ? errors.imageUrl : null}</p>
-
-                    </div>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>}
-
+            <div>
+              <input
+                type="text"
+                placeholder="Enter A Location"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                // required
+              ></input>
+              <p className="restaurant-errors">
+                {errors.location ? errors.location : null}
+              </p>
+            </div>
+            <div>
+              <select value={type} onChange={(e) => setType(e.target.value)}>
+                <option value={""} disabled selected>
+                  Select Type
+                </option>
+                {restaurantTypes.map((restaurant, idx) => (
+                  <option key={idx}>{restaurant}</option>
+                ))}
+              </select>
+              <p className="restaurant-errors">
+                {errors.type ? errors.type : null}
+              </p>
+            </div>
+            <div>
+              <input
+                type="url"
+                placeholder="Enter New Image Url"
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              ></input>
+              <p className="restaurant-errors">
+                {errors.imageUrl ? errors.imageUrl : null}
+              </p>
+            </div>
+            <button className="restaurant-submit" type="submit">
+              Submit
+            </button>
+          </form>
+          <img
+            className="res-logo"
+            src="https://i.postimg.cc/8cdCxDbc/restaurant-logo.jpg"
+            alt="res-logo"
+          />
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default UpdateRestaurant
+export default UpdateRestaurant;
